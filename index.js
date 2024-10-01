4a4300f649d96a31cd779305e2744697a999a4cd
iam-mayank-kansal <mkstarmayank221@gmail.com>
2024-10-01 19:30:47 +0530

    push

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+// connection with DB 
 const { connectionWithDBFunction } = require("./connectionDB");
 connectionWithDBFunction();
 
-app.get("/", (req, res) => {
-    res.end("Hello");
-})
+
+// PORT 
 app.listen(PORT, () => { console.log(`Server Running At Port ${PORT}`) });
